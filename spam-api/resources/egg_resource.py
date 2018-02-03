9d89046051a424233859ac915339aaea3ff3dd29
wray <wray@wrayesian.com>
2018-02-03 10:42:20 -0500

    Create egg_resource.py

---
+++ spam-api/resources/egg_resource.py
@@ -0,0 +1,17 @@
+from flask_restful import Resource, fields, marshal
+
+class Egg:
+    pass
+
+resources = {
+    'type' : fields.String(),
+    'cook' : fields.String()
+}
+
+class EggResource(Resource):
+	
+    def get(self):
+        e = Egg()
+        e.type = 'Chicken'
+	      e.cook = 'Scrambled'
+        return marshal(e,resources)
