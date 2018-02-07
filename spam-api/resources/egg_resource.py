6c2fd694f57752c7e646b054f190a229fd352f1f
wray <wray@wrayesian.com>
2018-02-07 16:45:17 -0500

    playing around

--- spam-api/resources/egg_resource.py
+++ spam-api/resources/egg_resource.py
@@ -5,13 +5,16 @@ class Egg:
 
 resources = {
     'type' : fields.String(),
-    'cook' : fields.String()
+    'cook' : fields.String(),
+    'sunny-side' : fields.Boolean(attribute='sunny_side')
 }
 
 class EggResource(Resource):
 	
     def get(self):
+        print('Ordering the egg.')
         e = Egg()
         e.type = 'Chicken'
-        e.cook = 'Scrambled'
+        e.cook = 'easy'
+        e.sunny_side = True
         return marshal(e,resources)
